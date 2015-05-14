3b6152a4da3e7be1140419e1785bbf0e3f767954
thedean11 <edean1010@gmail.com>
2015-05-13 23:59:14 -0500

    setup activate color scheme test environment

--- test/integration/test_activate_color_scheme.rb
+++ test/integration/test_activate_color_scheme.rb
@@ -1,53 +1,17 @@
 require_relative '../helper'
 
 
-class CreateANewColorSchemeTest < MiniTest::Test
+class ActivateExistingColorSchemeTest < MiniTest::Test
 
-    def test_blank_new_color_scheme_name
+    def test_invalid_activate_color_scheme
         shell_output = ""
         expected = ""
         IO.popen('././terminal_color_capture','r+') do |pipe|
             expected << menu_prompt
-            pipe.puts "1"
-            expected << "What would you like to call this color scheme?\n"
+            pipe.puts "2"
+            expected << "Which color scheme would you like to activate?\n"
             pipe.puts ""
-            expected << "You must enter a name for this color scheme.\n"
-            pipe.close_write
-            shell_output = pipe.read
-        end
-        assert_equal expected,shell_output
-    end
-
-    def test_invalid_text_color
-        shell_output = ""
-        expected = ""
-        IO.popen('././terminal_color_capture','r+') do |pipe|
-            expected << menu_prompt
-            pipe.puts "1"
-            expected << "What would you like to call this color scheme?\n"
-            pipe.puts "Test"
-            expected << "What color text would you like it to have?\n"
-            pipe.puts "Zurple"
-            expected << "You must choose one of [red, blue, green, yellow, black, white, orange, purple].\n?  "
-            pipe.close_write
-            shell_output = pipe.read
-        end
-        assert_equal expected,shell_output
-    end
-
-    def test_invalid_text_format
-        shell_output = ""
-        expected = ""
-        IO.popen('././terminal_color_capture','r+') do |pipe|
-            expected << menu_prompt
-            pipe.puts "1"
-            expected << "What would you like to call this color scheme?\n"
-            pipe.puts "Test"
-            expected << "What color text would you like it to have?\n"
-            pipe.puts "blue"
-            expected << "What format would you like it to have? (i.e. none, bold)\n"
-            pipe.puts "striking through it"
-            expected << "You must choose one of [none, bold, underline].\n?  "
+            expected << "You must enter a valid color scheme to activate.\n"
             pipe.close_write
             shell_output = pipe.read
         end
